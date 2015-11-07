c9616ccfe64811e5356a660690cb70121debfbfd
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-08 00:31:11 +0100

    godoc added

--- wms/carriers.go
+++ wms/carriers.go
@@ -1,25 +1,39 @@
 package wms
 
+import "errors"
+
+// A Carrier is some entity which knows where a package is. It has a
+// human-readable name and two methods: one to check if it might be responsible
+// for a given package ID; another to actually get the info for this package.
 type Carrier interface {
 	GetName() string
 	MatchPackage(p PackageID) bool
 	GetPackageInfo(p PackageID) (*PackageInfo, error)
 }
 
+// GenericCarrier is a convenient way to declare a new carrier.
 type GenericCarrier struct {
 	name           string
 	matchPackage   func(p PackageID) bool
 	getPackageInfo func(p PackageID) (*PackageInfo, error)
 }
 
+// GetName implements the Carrier interface.
 func (c GenericCarrier) GetName() string { return c.name }
+
+// MatchPackage implements the Carrier interface.
 func (c GenericCarrier) MatchPackage(p PackageID) bool {
 	if c.matchPackage == nil {
 		return false
 	}
 	return c.matchPackage(p)
 }
+
+// GetPackageInfo implements the Carrier interface.
 func (c GenericCarrier) GetPackageInfo(p PackageID) (*PackageInfo, error) {
+	if c.getPackageInfo == nil {
+		return nil, errors.New("undefined method: getPackageInfo")
+	}
 	return c.getPackageInfo(p)
 }
 
@@ -27,10 +41,12 @@ var _ Carrier = GenericCarrier{}
 
 var carriers = make(map[string]Carrier)
 
+// RegisterCarrier registers a carrier in the global register
 func RegisterCarrier(c Carrier) {
 	carriers[c.GetName()] = c
 }
 
+// GetCarrier gets a carrier by name from the global register
 func GetCarrier(name string) (Carrier, bool) {
 	c, ok := carriers[name]
 	if !ok {
@@ -39,6 +55,7 @@ func GetCarrier(name string) (Carrier, bool) {
 	return c, true
 }
 
+// GetCarriers returns a slice of all the carriers in the global register
 func GetCarriers() (cs []Carrier) {
 	for _, c := range carriers {
 		cs = append(cs, c)
@@ -47,6 +64,8 @@ func GetCarriers() (cs []Carrier) {
 	return
 }
 
+// GetCarrierForPackage returns the first carrier which might match the given
+// package ID. We don't support false positives for now.
 func GetCarrierForPackage(p PackageID) (Carrier, error) {
 	for _, c := range carriers {
 		if c.MatchPackage(p) {
--- wms/wms.go
+++ wms/wms.go
@@ -7,11 +7,13 @@ import (
 	"time"
 )
 
+// ErrUnknownCarrier is returned when no carrier can match a given package ID
 var ErrUnknownCarrier = errors.New("Unknown transporter for this package")
 
+// PackageID is a package ID
 type PackageID string
 
-// WIP
+// PackageInfo contains info describing the state of a package (WIP)
 type PackageInfo struct {
 	PackageID  PackageID
 	LastUpdate time.Time
